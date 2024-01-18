64dfffd39feec575882124de63a7e09052fd308d
Rfun-dev <erfan.ferdianto2003@gmail.com>
2024-01-18 20:57:13 +0700

    Initial commit

---
+++ app/src/main/java/com/example/bukuta/fragment/LogFragment.kt
@@ -0,0 +1,17 @@
+package com.example.bukuta.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.bukuta.R
+class LogFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_log, container, false)
+    }
+}
